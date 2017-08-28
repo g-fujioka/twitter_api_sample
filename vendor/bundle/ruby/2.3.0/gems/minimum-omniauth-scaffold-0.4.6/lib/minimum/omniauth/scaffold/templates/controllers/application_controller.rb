b13f3d54def58736d5431cb00ef984b5f57473bd
Gakuto Fujioka <g-fujioka@rhizome-e.com>
2017-08-28 15:42:06 +0900

    必要なgemを追加

---
+++ vendor/bundle/ruby/2.3.0/gems/minimum-omniauth-scaffold-0.4.6/lib/minimum/omniauth/scaffold/templates/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+  before_action :authenticate
+  before_action :reset_session_expires
+
+  private
+
+  def authenticate
+    unless signed_in?
+      session[:request_url] = request.url
+
+      redirect_to :root and return
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  def signed_in?
+    User.where(id: session[:user_id]).exists?
+  end
+  helper_method :signed_in?
---
+++ vendor/bundle/ruby/2.3.0/gems/minimum-omniauth-scaffold-0.4.6/lib/minimum/omniauth/scaffold/version.rb
@@ -0,0 +1,7 @@
+module Minimum
+  module Omniauth
+    module Scaffold
+      VERSION = '0.4.6'
+    end
+  end
+end
