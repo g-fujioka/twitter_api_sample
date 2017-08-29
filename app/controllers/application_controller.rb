eac927b70ed7bd074befd65294032247f215254c
Gakuto Fujioka <g-fujioka@rhizome-e.com>
2017-08-29 15:43:54 +0900

    twitterのpost機能を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate
+  # before_action :reset_session
+
+  private
+
+  def authenticate
+    unless signed_in?
+      session[:request_url] = request.url
+
+      redirect_to :root and return
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  def signed_in?
+    User.where(id: session[:user_id]).exists?
+  end
+  helper_method :signed_in?
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'top#index'
+
+  # For OmniAuth
+  get '/auth/:provider/callback', to: 'sessions#callback'
+  get '/auth/failure',            to: 'sessions#failure'
+  get '/logout',                  to: 'sessions#destroy', as: :logout
+
+  Rails.application.routes.draw do
+    # tweet
+    post '/top/tweet' => 'top#tweet', as: :tweet
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
