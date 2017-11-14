620dea4a6fb816ba4ce800c1ac1d089328f86317
Gakuto Fujioka <g-fujioka@rhizome-e.com>
2017-11-14 17:22:01 +0900

    timezoneを考慮

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module TwitterPostSample
       g.template_engine :haml
     end
 
+    config.time_zone = 'Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
