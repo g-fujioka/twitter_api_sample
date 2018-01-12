64b12bf50a6a9db1f34f0385cb9e9788528e61de
Gakuto Fujioka <g-fujioka@rhizome-e.com>
2018-01-12 16:47:07 +0900

    stream機能を使ってみる

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -42,16 +42,46 @@ class TopController < ApplicationController
   end
 
   def destroy
-    client.destroy(params[:id])
+    client.destroy_status(params[:tweet])
     redirect_to top_get_path, flash: {notice: '削除しました'}
-  rescue
-    redirect_to top_get_path, flash: {notice: '削除できませんでした'}
+  rescue => e
+    redirect_to top_get_path, flash: {notice: e}
+  end
+
+  def stream
+  end
+
+  def stream_ajax
+    # 現状ターミナルに垂れ流し
+    # @stream_text = []
+    stream_client.sample do |object|
+    #   @stream_text << object.text if object.is_a?(Twitter::Tweet)
+    #   if @stream_text.size > 100
+    #     respond_to do |format|
+    #       format.html
+    #       format.js
+    #     end
+    #   end
+      p object.text if object.is_a?(Twitter::Tweet)
+    end
   end
 
   private
 
   def client
     Twitter::REST::Client.new do |config|
+      # applicationの設定
+      config.consumer_key         = Settings.twitter_key
+      config.consumer_secret      = Settings.twitter_secret
+      # ユーザー情報の設定
+      user_auth = current_user.authentications.first
+      config.access_token         = user_auth.token
+      config.access_token_secret  = user_auth.secret
+    end
+  end
+
+  def stream_client
+    Twitter::Streaming::Client.new do |config|
       # applicationの設定
       config.consumer_key         = Settings.twitter_key
       config.consumer_secret      = Settings.twitter_secret
